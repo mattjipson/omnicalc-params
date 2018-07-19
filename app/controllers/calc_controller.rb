077cd10405eddc4de15f91cc953b150e380f1f9e
Matt Jipson <mjipson@chicagobooth.edu>
2018-07-19 00:23:47 +0000

    flex_payment

--- app/controllers/calc_controller.rb
+++ app/controllers/calc_controller.rb
@@ -17,5 +17,24 @@ class CalcController < ApplicationController
         
         render ("calc_templates/flexible_square_root.html.erb")
     end
+ 
+     def flex_payment
+        
+        @apr = params.fetch("apr").to_f/100
+        @years = params.fetch("years").to_f.round
+        @principal = params.fetch("principal").to_f
+        @payment = (@apr/100/12*@principal)/(1-(1+@apr/100/12)**(-@years*12))
+        
+        render ("calc_templates/flexible_payment.html.erb")
+    end
+
+     def flex_random
+        
+        @first = params.fetch("first").to_f.round
+        @second = params.fetch("second").to_f.round
+        @random = (@apr/100/12*@principal)/(1-(1+@apr/100/12)**(-@years*12))
+        
+        render ("calc_templates/flexible_payment.html.erb")
+    end
     
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,8 @@ Rails.application.routes.draw do
     
     get("/flexible/square_root/:the_users_num", { :controller => "calc", :action => "flex_root"})
 
+    get("/flexible/payment/:apr/:years/:principal", { :controller => "calc", :action => "flex_payment"})
+
+    get("/flexible/random/:first/:second", { :controller => "calc", :action => "flex_random"})
+
 end
